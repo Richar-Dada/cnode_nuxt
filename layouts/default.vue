2910b48107fcd73ea31d9ace1c6cc2c8e24862bc
zicong <zicong@igola.com>
2017-12-19 23:16:48 +0800

    Push  add muse-ui

---
+++ layouts/default.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <mu-appbar title="Title">
+      <mu-icon-button icon="menu" slot="left"/>
+      <mu-flat-button label="expand_more" slot="right"/>
+      <mu-icon-button icon="expand_more" slot="right"/>
+    </mu-appbar>
+    <nuxt/>
+    <my-footer/>
+  </div>
+</template>
+
+<script>
+import MyFooter from '~/components/Footer.vue'
+
+export default {
+  components: {
+    MyFooter
+  }
+}
+</script>
+
+<style>
+.container
+{
+  margin: 0;
+  width: 100%;
+  padding: 100px 0;
+  text-align: center;
+}
+
+.button, .button:visited
+{
+  display: inline-block;
+  color: black;
+  letter-spacing: 1px;
+  background-color: #fff;
+  border: 2px solid #000;
+  text-decoration: none;
+  text-transform: uppercase;
+  padding: 15px 45px;
+}
+
+.button:hover, .button:focus
+{
+  color: #fff;
+  background-color: #000;
+}
+
+.title
+{
+  color: #000;
+  font-weight: 300;
+  font-size: 2.5em;
+  margin: 0;
+}
+</style>
---
+++ nuxt.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'starter',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/icon?family=Material+Icons'
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://unpkg.com/muse-ui@2.1.0/dist/muse-ui.css'
+      }
+    ]
+  },
+  /*
+  ** Global CSS
+  */
+  css: ['~/assets/css/main.css'],
+  /*
+  ** Add axios globally
+  */
+  build: {
+    vendor: [
+      'axios',
+      'muse-ui'
+    ],
+    /*
+    ** Run ESLINT on save
+    */
+    extend (config, ctx) {
+      if (ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  },
+  plugins: ['~/plugins/museui']
+}
