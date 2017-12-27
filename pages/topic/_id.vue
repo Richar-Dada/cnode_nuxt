fd1b18d4fc2c85aa24c96077203c2e58271ca700
zicong <zicong@igola.com>
2017-12-27 21:59:31 +0800

    Push  显示评论内容

--- pages/topic/_id.vue
+++ pages/topic/_id.vue
@@ -1,14 +1,28 @@
 <template>
-  <mu-card class="article">
-    <mu-card-header :title="article.author.loginname" subTitle="sub title">
-      <mu-avatar :src="article.author.avatar_url" slot="avatar"/>
-    </mu-card-header>
-    <mu-divider/>
-    <mu-card-title :title="article.title" :subTitle="'发布于' + article.create_at.slice(0, 10) + '   ' + article.visit_count + '次阅读'"/>
-    <mu-card-text>
-      <div v-html="article.content"></div>
-    </mu-card-text>
-  </mu-card>
+  <div>
+    <mu-card class="article">
+      <mu-card-header :title="article.author.loginname" subTitle="sub title">
+        <mu-avatar :src="article.author.avatar_url" slot="avatar"/>
+      </mu-card-header>
+      <mu-divider/>
+      <mu-card-title :title="article.title" :subTitle="'发布于' + article.create_at.slice(0, 10) + '   ' + article.visit_count + '次阅读'"/>
+      <mu-card-text>
+        <div v-html="article.content"></div>
+      </mu-card-text>
+    </mu-card>
+    <div v-for="(reply, index) in article.replies" :key="reply.id" @click="">
+      <mu-flexbox>
+        <mu-flexbox-item class="flex-demo avatar-box" grow="1">
+          <mu-avatar :src="reply.author.avatar_url"/>
+        </mu-flexbox-item>
+        <mu-flexbox-item class="flex-demo" grow="11">
+          <p>{{ reply.author.loginname }}</p>
+          <div v-html="reply.content"></div>
+        </mu-flexbox-item>
+      </mu-flexbox>
+      <mu-divider/>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -26,5 +40,9 @@
   .article img {
     max-width: 100%;
     height: auto;
+  }
+  .avatar-box{
+    margin-bottom: 0;
+    text-align: center;
   }
 </style>
